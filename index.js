fae5e18ca91f0ca090ff1bd07c50e3a0c3c067be
Jeff Beaty <beaty3371@gmail.com>
2025-04-15 06:29:09 -0400

    Fix import path for formdata.model.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.error('MongoDB error:', err));
 
 // Load Models
-const FormData = require('./FormData.model');
+const FormData = require('./formdata.model');
 const Facility = require('./Facility.model'); // NEW
 
 // Delete entry
